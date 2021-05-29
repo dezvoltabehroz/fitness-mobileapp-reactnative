bae7a726f256c6dfb19d88881ba052807364329a
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2021-05-29 15:48:25 +0500

    hot fixes

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, TouchableOpacity, ScrollView, Text, Image, Dimensions, FlatList } from 'react-native';
-import { Icon } from '../index';
+// import { Icon } from '../index';
+import { Icon } from 'native-base'
 import THEME from '../../assets/styles/theme.style';
 import Modal from 'react-native-modal';
 import styles from './style';
@@ -11,48 +12,52 @@ const Container = ({ children, props }) => {
 
     const array = [{
         name: "Hub",
-        iconName: <Icon.Entypo name={'gauge'} size={20} color='white' />,
+        type: "Entypo", iconName: 'gauge'
     },
     {
         name: "Workouts",
-        iconName: <Icon.FontAwesome5 name={'fire-alt'} size={20} color='white' />,
+        type: "FontAwesome5", iconName: 'fire-alt'
     },
     {
         name: "Nutrition",
-        iconName: <Icon.MaterialIcons name={'dinner-dining'} size={25} color='white' />,
+        type: "MaterialIcons", iconName: 'dinner-dining'
     },
     {
         name: "Programs",
-        iconName: <Icon.FontAwesome5 name={'calendar-alt'} size={20} color='white' />,
+        type: "FontAwesome5", iconName: 'calendar-alt'
     },
     {
         name: "Calendar",
-        iconName: <Icon.FontAwesome5 name={'calendar-day'} size={20} color='white' />,
+        type: "FontAwesome5", iconName: 'calendar-day'
     },
     {
         name: "Chat",
-        iconName: <Icon.Ionicons name={'ios-chatbox-ellipses-sharp'} size={20} color='white' />,
+        type: "Ionicons", iconName: 'ios-chatbox-ellipses-sharp'
     },
     {
         name: "Financials",
-        iconName: <Icon.MaterialCommunityIcons name={'sack'} size={25} color='white' />,
+        type: "MaterialCommunityIcons", iconName: 'sack'
     },
     {
         name: "Marketplace",
-        iconName: <Icon.MaterialCommunityIcons name={'bookmark'} size={25} color='white' />,
+        type: "MaterialCommunityIcons",
+        iconName: 'bookmark'
     },
     {
         name: "Files",
-        iconName: <Icon.FontAwesome name={'file-o'} size={20} color='white' />,
+        type: "FontAwesome",
+        iconName: 'file-o'
 
     },
     {
         name: "Forms",
-        iconName: <Icon.FontAwesome name={'wpforms'} size={20} color='white' />,
+        type: "FontAwesome",
+        iconName: 'wpforms'
     },
     {
         name: "Settings",
-        iconName: <Icon.FontAwesome name={'gears'} size={20} color='white' />,
+        type: "FontAwesome",
+        iconName: 'gears',
     }]
 
     const _renderItems = ({ index, item }) => {
@@ -64,7 +69,7 @@ const Container = ({ children, props }) => {
                 style={{ alignItems: "center" }}>
                 <View onPress={() => {
                 }} style={[styles.flatlistContainer, { backgroundColor: "#544b4c" }]}>
-                    {item.iconName}
+                    <Icon type={item.type} name={item.iconName} style={{ fontSize: 20, color: 'white' }} />
                 </View>
                 <View style={{ marginTop: 12 }}>
                     <Text style={{ fontSize: 12 }} >{item.name}</Text>
@@ -93,7 +98,7 @@ const Container = ({ children, props }) => {
 
                         </View>
                         <TouchableOpacity onPress={() => props.authActions.menuModal(!props.user.menuModal)} style={{ marginHorizontal: "5%", height: 30, width: 40 }}>
-                            <Icon.MaterialCommunityIcons name="dialpad" size={40} />
+                            <Icon type="MaterialCommunityIcons" name="dialpad" style={{ fontSize: 40 }} />
                         </TouchableOpacity>
                     </View>
                 </View>
@@ -108,7 +113,7 @@ const Container = ({ children, props }) => {
                             <View style={{ flex: 0.5 }}>
                                 <View style={{ marginTop: '5%', flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}  >
                                     <TouchableOpacity onPress={() => props.authActions.menuModal(!props.user.menuModal)} style={{ marginHorizontal: "5%" }}>
-                                        <Icon.Fontisto name="bell-alt" size={25} color="#544b4c" />
+                                        <Icon type="Fontisto" name="bell-alt" size={25} color="#544b4c" />
                                     </TouchableOpacity>
                                     <TouchableOpacity onPress={() => props.authActions.menuModal(!props.user.menuModal)} style={{ marginHorizontal: "5%", justifyContent: "center", alignItems: "center", height: 30, width: 30, borderRadius: 15, backgroundColor: "#544b4c" }}>
                                         <Text style={{ color: "white", fontWeight: "bold" }} >T</Text>
@@ -136,8 +141,8 @@ const Container = ({ children, props }) => {
                             <View>
 
                             </View>
-                            <TouchableOpacity onPress={() => props.authActions.menuModal(!props.user.menuModal)} style={{ marginHorizontal: "5%", height: 30, width: 40 }}>
-                                <Icon.MaterialCommunityIcons name="dialpad" size={40} />
+                            <TouchableOpacity onPress={() => props.authActions.menuModal(!props.user.menuModal)} style={{ marginHorizontal: "5%", height: 30, width: 40, overflow: "hidden" }}>
+                                <Icon type="MaterialCommunityIcons" name="dialpad" style={{ fontSize: 40 }} />
                             </TouchableOpacity>
                         </View>
                     </View>
