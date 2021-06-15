cd93550337f9f65805396d7024b08ef537c879bf
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2021-06-15 21:12:36 +0500

    hot fixes

--- src/screens/Chat/index.js
+++ src/screens/Chat/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, ScrollView, Dimensions } from 'react-native';
+import { View, Text, FlatList, ScrollView, Dimensions, TextInput, TouchableOpacity, Image } from 'react-native';
 import { Container, FilterModal, Icon } from '../../components';
 import styles from './style';
 import { connect } from 'react-redux'
@@ -14,19 +14,136 @@ class Media extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            progress: 100,
-            filterModal: false
+            messages: [
+                {
+                    text: 'Some Text Mesage',
+                    align: 'left',
+                },
+                {
+                    text: 'Some Long Text Mesage',
+                    align: 'right',
+                },
+                {
+                    text: 'Some Text Mesage',
+                    align: 'left',
+                },
+                {
+                    text: 'Some Text Mesage',
+                    align: 'left',
+                },
+                {
+                    text: 'Some Long Text Mesage',
+                    align: 'right',
+                },
+                {
+                    text: 'Some Text Mesage',
+                    align: 'left',
+                },
+                {
+                    text: 'Some Long Text Mesage',
+                    align: 'right',
+                },
+                {
+                    text: 'Some Long Text Mesage',
+                    align: 'right',
+                },
+            ],
+            attachment: false
         }
     }
 
-    render() {
-        const { progress, reportModal, issue, filterModal } = this.state;
-       
+    _renderSeparator = () => {
         return (
-            <View style={styles.container}>
+            <View style={styles.seperatorStyle}></View>
+        )
+    }
+
+    _renderItems = (item) => {
+        return (
+            <>
+                <View style={{ flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center', alignSelf: item.align == 'right' ? 'flex-end' : 'flex-start', marginLeft: item.align == 'right' ? 0 : 10 }}>
+                    {
+                        item.align == 'left' ?
+                            <Image source={require('../../assets/images/logo.png')} resizeMode="cover" style={{ height: 50, width: 40 }} />
+                            :
+                            null
+                    }
+                    <View
+                        style={[styles.balloon, {
+                            borderBottomLeftRadius: item.align == 'left' ? 0 : 20,
+                            borderBottomRightRadius: item.align == 'right' ? 0 : 20,
+                            marginLeft: item.align == 'left' ? 5 : 0,
+                            marginRight: item.align == 'right' ? 6 : 0,
+                            backgroundColor: item.align == 'left' ? 'gray' : 'lightgray',
+                            alignSelf: item.align == 'right' ? 'flex-end' : 'flex-start',
+                            flexDirection: 'row'
+                        }]}
+                    >
+
+                        <View>
+                            <Text style={[styles.item, { paddingBottom: 10 }]}>{item.text}</Text>
+                        </View>
 
 
-            </View>
+                    </View>
+
+
+                </View>
+            </>
+        )
+    }
+
+    render() {
+        return (
+            <>
+
+                <View style={{ flex: 1, paddingTop: '2.5%', marginBottom: 10 }}>
+                    <FlatList
+                        data={this.state.messages}
+                        showsVerticalScrollIndicator={false}
+                        contentContainerStyle={{ justifyContent: "flex-end" }}
+                        ItemSeparatorComponent={this._renderSeparator}
+                        renderItem={({ item }) => this._renderItems(item)}
+                        keyExtractor={item => item}
+
+                    />
+                </View>
+                <View style={styles.attachment}>
+                    {
+                        this.state.attachment ?
+                            <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+                                <Icon.Ionicons name='happy' size={30} style={{ color: 'lightgray' }} />
+                                <Icon.Entypo name='attachment' size={30} style={{ color: 'lightgray' }} />
+                                <View style={{ backgroundColor: "rgb(30,199,178)", borderRadius: 30, padding: 2.5 }}>
+                                    <Icon.Ionicons name='md-paper-plane' size={30} style={{ color: 'white' }} />
+                                </View>
+                            </View>
+                            :
+                            null
+                    }
+                    <View style={[styles.inputBoxView]}>
+                        <TouchableOpacity onPress={() => this.setState({ attachment: !this.state.attachment })} style={{ padding: 7.5 }}>
+                            <Icon.Entypo name='plus' size={30} style={{ color: 'lightgray' }} />
+                        </TouchableOpacity>
+                        <Icon.Ionicons name='mic' size={30} style={{ color: 'lightgray' }} />
+                        <TextInput style={styles.commentInput}
+                            placeholder="  Type here..."
+                            // multiline={true}
+                            // placeholderTextColor=""
+                            // value={this.state.txtMessage}
+                            onChangeText={text => {
+                                this.setState({ txtMessage: text })
+                            }}
+                            selectionColor="#FFF"
+                        />
+                        <Icon.Ionicons name='md-paper-plane' size={30} style={{ color: 'gray' }} />
+                    </View>
+                </View>
+
+
+
+
+            </>
         )
     }
 }
--- src/screens/Chat/style.js
+++ src/screens/Chat/style.js
@@ -8,5 +8,61 @@ export default StyleSheet.create({
         flex: 1,
         backgroundColor: THEME.COLOR_WHITE,
     },
-    
+    inputBoxView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        // backgroundColor: 'white',
+        borderRadius: 25,
+        paddingTop: Platform.OS === "android" ? 0 : 3,
+        paddingBottom: Platform.OS === "android" ? 0 : 3,
+        overflow: "hidden",
+        marginRight: 5,
+        marginLeft: 5,
+        paddingRight: 10,
+        paddingLeft: 10
+        // paddingVertical: '5%',
+        // minHeight: 47,
+    },
+    commentInput: {
+        color: 'black',
+        fontSize: 14,
+        lineHeight: 18,
+        flex: 1,
+        paddingTop: 12,
+        paddingBottom: 10,
+
+        // maxHeight: 150,
+    },
+    balloon: {
+        alignSelf: 'baseline',
+        paddingHorizontal: 5,
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderRadius: 18,
+        maxWidth: '80%',
+        minWidth: '20%',
+        marginRight: 13,
+    },
+    item: {
+        letterSpacing: 0.5,
+        lineHeight: 20,
+        padding: 10,
+        fontSize: 14,
+        // color: ,
+        flex: 1
+    },
+    seperatorStyle: {
+        height: 15
+    },
+    attachment: {
+        // flexDirection: 'row',
+        // alignItems: 'center',
+        justifyContent: 'center',
+        // backgroundColor: 'white',
+        borderRadius: 30,
+        // marginRight: 10,
+        // marginLeft: 10,
+        // paddingTop: 10,
+        // marginBottom: 15,
+    }
 })
