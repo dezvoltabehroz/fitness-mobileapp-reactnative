ece90fd4f66ee6f9b92bcdb8fc289befb8569264
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2021-06-15 19:10:31 +0500

    hot fixes

--- src/screens/Calendar/index.js
+++ src/screens/Calendar/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, ScrollView, Dimensions, Image } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, ScrollView, Dimensions, Image } from 'react-native';
 import { Container, FilterModal, Icon } from '../../components';
 import styles from './style';
 import { connect } from 'react-redux'
@@ -10,14 +10,71 @@ import RNBounceable from '@freakycoder/react-native-bounceable';
 import moment from 'moment';
 import ProgressBarAnimated from 'react-native-progress-bar-animated';
 import Modal from 'react-native-modal';
-
+import { Agenda } from 'react-native-calendars';
+const testIDs = require('../../config/testIDs');
 class Calendar extends Component {
     constructor(props) {
         super(props);
         this.state = {
             progress: 100,
-            filterModal: false
+            filterModal: false,
+            items: {}
         }
+    }
+
+    loadItems(day) {
+        setTimeout(() => {
+            for (let i = -15; i < 85; i++) {
+                const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+                const strTime = this.timeToString(time);
+                if (!this.state.items[strTime]) {
+                    this.state.items[strTime] = [];
+                    const numItems = Math.floor(Math.random() * 3 + 1);
+                    for (let j = 0; j < numItems; j++) {
+                        this.state.items[strTime].push({
+                            name: 'Item for ' + strTime + ' #' + j,
+                            height: Math.max(50, Math.floor(Math.random() * 150))
+                        });
+                    }
+                }
+            }
+            const newItems = {};
+            Object.keys(this.state.items).forEach(key => {
+                newItems[key] = this.state.items[key];
+            });
+            this.setState({
+                items: newItems
+            });
+        }, 1000);
+    }
+
+    renderItem(item) {
+        return (
+            <TouchableOpacity
+                testID={testIDs.agenda.ITEM}
+                style={[styles.item, { height: item.height }]}
+                onPress={() => Alert.alert(item.name)}
+            >
+                <Text>{item.name}</Text>
+            </TouchableOpacity>
+        );
+    }
+
+    renderEmptyDate() {
+        return (
+            <View style={styles.emptyDate}>
+                <Text>This is empty date!</Text>
+            </View>
+        );
+    }
+
+    rowHasChanged(r1, r2) {
+        return r1.name !== r2.name;
+    }
+
+    timeToString(time) {
+        const date = new Date(time);
+        return date.toISOString().split('T')[0];
     }
 
     render() {
@@ -30,9 +87,29 @@ class Calendar extends Component {
                             <Text style={styles.headingStyle}>{"Calendar"}</Text>
                         </View>
                         <View style={styles.lowerContentContainer}>
-                            <ScrollView contentContainerStyle={{ paddingBottom: 120 }}>
-
-                            </ScrollView>
+                            <Agenda
+                                testID={testIDs.agenda.CONTAINER}
+                                items={this.state.items}
+                                loadItemsForMonth={this.loadItems.bind(this)}
+                                selected={'2021-06-14'}
+                                renderItem={this.renderItem.bind(this)}
+                                renderEmptyDate={this.renderEmptyDate.bind(this)}
+                                rowHasChanged={this.rowHasChanged.bind(this)}
+                            // markingType={'period'}
+                            // markedDates={{
+                            //    '2017-05-08': {textColor: '#43515c'},
+                            //    '2017-05-09': {textColor: '#43515c'},
+                            //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+                            //    '2017-05-21': {startingDay: true, color: 'blue'},
+                            //    '2017-05-22': {endingDay: true, color: 'gray'},
+                            //    '2017-05-24': {startingDay: true, color: 'gray'},
+                            //    '2017-05-25': {color: 'gray'},
+                            //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+                            // monthFormat={'yyyy'}
+                            // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+                            //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+                            // hideExtraDays={false}
+                            />
 
                         </View>
 
@@ -81,7 +158,7 @@ class Calendar extends Component {
                         </RNBounceable>
                     </View>
                 </Modal>
-                
+
             </>
         )
     }
